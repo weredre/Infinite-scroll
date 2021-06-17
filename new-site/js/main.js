a4d679a4619f32f6816e02dc60305aab21020ada
Andre Heslop <andreheslop@gmail.com>
2021-06-16 21:11:12 -0400

    infinite scroll working

--- new-site/js/main.js
+++ new-site/js/main.js
@@ -1,17 +1,23 @@
 // Get an API key for your demos at https://unsplash.com/developers
-const unsplashID = 'yJiR4Eu1pytQ-zXV5s4jGrTWdQF0955S4v_3pwdWM6M';
+// const unsplashID = 'yJiR4Eu1pytQ-zXV5s4jGrTWdQF0955S4v_3pwdWM6M';
+
+// fetch('./fuck-this.json')
+//   .then(response => response.json())
 
 let $container = $( '.container').infiniteScroll({
-  path: function() {
-    return `https://api.unsplash.com/photos?client_id=${unsplashID}&page=${this.pageIndex}`;
+
+   path: function() {
+    return `https://api.jsonbin.io/b/60ca9f7e8ea8ec25bd0e98da`;
   },
   // load response as JSON
   responseBody: 'json',
   status: '.scroll-status',
   history: false,
+  debug:true
 });
 
 $container.on( 'load.infiniteScroll', function( event, body ) {
+
   // compile body data into HTML
   let itemsHTML = body.map( getItemHTML ).join('');
   // convert HTML string into elements
@@ -25,11 +31,11 @@ $container.infiniteScroll('loadNextPage');
 
 //------------------//
 
-function getItemHTML({ user, urls }) {
+function getItemHTML({ images, pinner,description }) {
   return `<div class="photo-item">
-    <img class="photo-item__image" src="${urls.regular}" alt="Photo by ${user.name}" />
+    <img class="photo-item__image" src="${images.orig.url}" alt="Photo by ${pinner.full_name}" />
     <p class="photo-item__caption">
-      <a href="${user.links.html}?utm_source=infinite-scroll-demos&utm_medium=referral&utm_campaign=api-credit">${user.name}</a>
-    </p>
+      <a href="${description}?utm_source=infinite-scroll-demos&utm_medium=referral&utm_campaign=api-credit">${pinner.full_name}</a>
+      </p>
   </div>`;
 }
